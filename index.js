c64bf983be8eae7f6b83883b2c3381ba19be6975
Mohamed Salah <mohamedsalahmasoud173@gmail.com>
2023-06-17 15:56:14 +0300

    connect to mogodb

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
+/* eslint-disable no-unused-vars */
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const MONGO_URL = process.env.MONGO_URL || 'mongodb://localhost:27017/auth';
+
+mongoose.connect(MONGO_URL);
+
+app.use('*', (req, res) => {
+  res.send('Error').status(400);
+});
 
 app.listen(PORT, () => {
   console.log(`listen on port ${PORT}`);
